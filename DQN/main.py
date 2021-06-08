9aeb918f22d3353dc25391c0e825a2b0d4437cc9
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-06-08 10:24:16 +0900

    fix

--- DQN/main.py
+++ DQN/main.py
@@ -1,35 +1,23 @@
 import random
 from collections import namedtuple
-from itertools import count
-import math
 import time
 
 import gym
-import matplotlib
-import matplotlib.pyplot as plt
+from gym import wrappers
 import numpy as np
-from PIL import Image
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
-# from utils import save
-
 np.random.seed(283)
 
-# 学習に使う変数を整理
 ENV = 'MountainCar-v0'
-# 報酬割引率
 GAMMA = 0.9
-# 1試行（1エピソード）の最大ステップ数
 MAX_STEP = 200
-# 最大試行回数（エピソード数）
 NUM_EPISODES = 10000
-# バッチサイズ32
 BATCH_SIZE = 64
-# キャパ
 CAPACITY = 10000
 
 Transition = namedtuple('Transition',
@@ -55,9 +43,7 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
-# エージェントが行う行動を与えられた状態によって判断する部分（深層強化学習（DQN）を行う部分）
 class Brain:
-
     def __init__(self, num_states, num_actions):
         self.num_actions = num_actions
         self.memory = ReplayMemory(CAPACITY)
@@ -67,57 +53,39 @@ class Brain:
         self.model.add_module('relu1', nn.ReLU())
         self.model.add_module('fc2', nn.Linear(32, 32))
         self.model.add_module('relu2', nn.ReLU())
-        self.model.add_module('fc3', nn.Linear(32, num_actions))
-
+        self.model.add_module('fc3', nn.Linear(32, 32))
+        self.model.add_module('relu3', nn.ReLU())
+        self.model.add_module('fc4', nn.Linear(32, 32))
+        self.model.add_module('relu4', nn.ReLU())
+        self.model.add_module('fc5', nn.Linear(32, num_actions))
         print(self.model)
-        # 最適化手法
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
 
-    # 結合パラメータを学習する部分
     def replay(self):
 
-        # 最初にメモリサイズを確認する
-        # 指定したバッチサイズより小さい場合は何もしない
         if len(self.memory) < BATCH_SIZE:
             return
-        # ミニバッチ用のデータを取得（ランダム）
         transitions = self.memory.sample(BATCH_SIZE)
-        # transitions は (state, action, next_state, reward) * BATCH_SIZE
-        # (state * BATCH_SIZE, action * BATCH_SIZE, next_state * BATCH_SIZE, reward * BATCH_SIZE)
         batch = Transition(*zip(*transitions))
 
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
         non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-        # 教師信号となるQ(s_t, a_t)を求める
         # モデルを推論モードに
         self.model.eval()
-        # self.model(state_batch)は，2つのQ値を出力する
-        # [torch.FloatTensor of size BATCH_SIZE * 2]になってるので
-        # 実行したアクション（a_t）に対応するQ値をaction_batchで行った行動a_tのindexを使って取得する
         state_action_values = self.model(state_batch).gather(1, action_batch)
-        # CartPole がdoneになっていない，かつ，next_stateがあるかをチェックするためのマスクを作成
-        # non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, batch.next_state)))
         non_final_mask = torch.BoolTensor(tuple(map(lambda s: s is not None, batch.next_state)))
-        # maxQ(s_t+1, a)を求める
         next_state_values = torch.zeros(BATCH_SIZE)
-        # 次の状態があるindexの最大Q値を求める
         next_state_values[non_final_mask] = self.model(non_final_next_states).max(1)[0].detach()
-        # Q学習の行動価値関数更新式からQ(S_t, a_t)を求める
         expected_state_action_values = reward_batch + GAMMA * next_state_values
         # モデルを訓練モードに切り替え
         self.model.train()
-        # 二乗誤差の代わりにHuber関数を使う
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-        # 勾配をリセット
         self.optimizer.zero_grad()
-        # 誤差逆伝搬
         loss.backward()
-        # ニューラルネットワークの重み更新
         self.optimizer.step()
 
-    # 現在の状態に応じて行動を決定する
     def decide_action(self, state, episode):
         # ε-greedy法で徐々に最適行動を採用するようにする
         epsilon = 0.5 * (1 / (episode + 1))
@@ -125,113 +93,86 @@ class Brain:
         if epsilon <= np.random.uniform(0, 1):
             # 推論モードに
             self.model.eval()
-            # ネットワークの出力の最大値のindexを取得
-            # view関数で行列サイズを（1 * 1）に調整
             with torch.no_grad():
                 action = self.model(state).max(1)[1].view(1, 1)
         else:
-            # 右，左ランダムに行動する
-            # actionは[torch.LongTensor of size 1 * 1]
             action = torch.LongTensor([[random.randrange(self.num_actions)]])
 
         return action
 
-# エージェントクラス
+
 class Agent:
     def __init__(self, num_states, num_actions):
-        # Brainクラスをインスタンス化
+
         self.brain = Brain(num_states, num_actions)
-    # Q関数の更新
+
     def update_q_function(self):
         self.brain.replay()
-    # アクションを決定する
+
     def get_action(self, state, episode):
         action = self.brain.decide_action(state, episode)
         return action
-    # 状態を保存
+
     def memorize(self, state, action, next_state, reward):
         self.brain.memory.push(state, action, next_state, reward)
 
-# CartPoleを実行する環境クラス
 class Environment:
 
     def __init__(self):
         self.env = gym.make(ENV)
+        self.env = wrappers.Monitor(self.env, './log/', force=True, video_callable=(lambda episode: episode % 10 == 0))
         num_states = self.env.observation_space.shape[0]
         num_actions = self.env.action_space.n
         self.agent = Agent(num_states, num_actions)
 
     def run(self):
-
-        episode_10_list = np.zeros(10) # 10試行分の立ち続けた平均ステップ数の出力に使う
-        complete_episodes = 0  # 旗まで連続して到達した数
-        is_episode_final = False  # 最終試行フラグ
-        frames = []  # 動画用に画像を格納する変数
+        episode_10_list = np.zeros(10)
+        complete_episodes = 0
         before = time.time()
-
         # 全エピソードループ
         for episode in range(NUM_EPISODES):
             # エピソード毎に環境を初期化
             observation = self.env.reset()
             state = observation
-            # numpyからpytorchのテンソルに変換
             state = torch.from_numpy(state).type(torch.FloatTensor)
-            # size を 1*4 に変換
             state = torch.unsqueeze(state, 0)
 
             for step in range(MAX_STEP):
-                # 最終試行はframesに画像を追加しておく
-                if is_episode_final:
-                    frames.append(self.env.render(mode='rgb_array'))
-                # 最初の行動を決める
+                self.env.render()
                 action = self.agent.get_action(state, episode)
-                # 最初の行動から次の状態を求める
-                observation_next, _, done, _ = self.env.step(action.item())
+                observation_next, reward, done, _ = self.env.step(action.item())
+                print('Episode: {0}, step: {1}, reward: {2}'.format(episode, step, reward))
                 # 報酬を与える
                 if done:
-                    # 次の状態はないのでNoneを代入
                     state_next = None
                     # 直前10エピソードで立てた平均ステップ数を格納
                     episode_10_list = np.hstack((episode_10_list[1:], step+1))
-                    if step < 199:
-                        # 規定回数までに到達できたら+200 ←特に深い理由はない(見栄えの問題)
-                        reward = torch.FloatTensor([200])
-                        # 連続成功回数を+1
+                    reward = torch.FloatTensor([reward])
+                    if step < MAX_STEP - 1:
                         complete_episodes += 1
+                        break
                     else:
-                        # 立ったまま終了した場合は報酬-1
-                        reward = torch.FloatTensor([-1.0])
-                        # 連続成功回数をリセット
                         complete_episodes = 0
+                        break
                 else:
-                    # 途中の報酬は-1
-                    reward = torch.FloatTensor([-1.0])
+                    reward = torch.FloatTensor([reward])
                     state_next = observation_next
                     state_next = torch.from_numpy(state_next).type(torch.FloatTensor)
                     state_next = torch.unsqueeze(state_next, 0)
 
-                # メモリに経験を追加
                 self.agent.memorize(state, action, state_next, reward)
-                # Q関数をニューラルネットで更新
                 self.agent.update_q_function()
-                # 状態を次の状態に更新
                 state = state_next
-                # エピソード終了時
-                if done:
+                if done or step==MAX_STEP:
                     print('{0}エピソード: {1}ステップで終了 - reward: {2}'.format(episode, step, reward))
                     break
-
-            # 最終エピソードの場合は動画を保存
-            # if is_episode_final:
-            #     save_as_gif(frames)
-            #     break
-            # 10回連続で成功したら、次のエピソードで終わりにする
-            if complete_episodes >= 10:
+            # 10回連続で成功したら終わりにする
+            if complete_episodes == 10:
                 after = time.time()
                 exe_time = after - before
                 print('10回連続成功')
                 print('Execution Time: {0} sec'.format(round(exe_time, 5)))
-                is_episode_final = True
+                return
 
 cartpole_env = Environment()
 cartpole_env.run()
--- dqn.py
+++ dqn.py
@@ -1,179 +1,159 @@
-import random
-from collections import namedtuple
-import math
+import copy
 import time
-
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
 import gym
 from gym import wrappers
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
-import torchvision.transforms as T
+import torch.optim as optim
+from torch.autograd import Variable
 
-np.random.seed(283)
+# 環境
+MONITOR = False
+env = gym.make("CartPole-v0")
+if MONITOR:
+    env = wrappers.Monitor(env, "./tmp", force=True)
 
-ENV = 'CartPole-v0'
-GAMMA = 0.9
-MAX_STEP = 500
-NUM_EPISODES = 10000
-BATCH_SIZE = 64
-CAPACITY = 10000
+obs_num = env.observation_space.shape[0]
+acts_num = env.action_space.n
+HIDDEN_SIZE = 100
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-class Brain:
-    def __init__(self, num_states, num_actions):
-        self.num_actions = num_actions
-        self.memory = ReplayMemory(CAPACITY)
-        # ニューラルネットワーク
-        self.model = nn.Sequential()
-        self.model.add_module('fc1', nn.Linear(num_states, 32))
-        self.model.add_module('relu1', nn.ReLU())
-        self.model.add_module('fc2', nn.Linear(32, 32))
-        self.model.add_module('relu2', nn.ReLU())
-        self.model.add_module('fc3', nn.Linear(32, 32))
-        self.model.add_module('relu3', nn.ReLU())
-        self.model.add_module('fc4', nn.Linear(32, 32))
-        self.model.add_module('relu4', nn.ReLU())
-        self.model.add_module('fc5', nn.Linear(32, num_actions))
-        print(self.model)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
-
-    def replay(self):
-
-        if len(self.memory) < BATCH_SIZE:
-            return
-        transitions = self.memory.sample(BATCH_SIZE)
-        batch = Transition(*zip(*transitions))
-
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-        # モデルを推論モードに
-        self.model.eval()
-        state_action_values = self.model(state_batch).gather(1, action_batch)
-        non_final_mask = torch.BoolTensor(tuple(map(lambda s: s is not None, batch.next_state)))
-        next_state_values = torch.zeros(BATCH_SIZE)
-        next_state_values[non_final_mask] = self.model(non_final_next_states).max(1)[0].detach()
-        expected_state_action_values = reward_batch + GAMMA * next_state_values
-        # モデルを訓練モードに切り替え
-        self.model.train()
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-    def decide_action(self, state, episode):
-        # ε-greedy法で徐々に最適行動を採用するようにする
-        epsilon = 0.5 * (1 / (episode + 1))
-
-        if epsilon <= np.random.uniform(0, 1):
-            # 推論モードに
-            self.model.eval()
-            with torch.no_grad():
-                action = self.model(state).max(1)[1].view(1, 1)
-        else:
-            action = torch.LongTensor([[random.randrange(self.num_actions)]])
-
-        return action
-
-
-class Agent:
-    def __init__(self, num_states, num_actions):
-
-        self.brain = Brain(num_states, num_actions)
-
-    def update_q_function(self):
-        self.brain.replay()
-
-    def get_action(self, state, episode):
-        action = self.brain.decide_action(state, episode)
-        return action
-
-    def memorize(self, state, action, next_state, reward):
-        self.brain.memory.push(state, action, next_state, reward)
-
-class Environment:
-
+class NN(nn.Module):
     def __init__(self):
-        self.env = gym.make(ENV)
-        self.env = wrappers.Monitor(self.env, './log/', force=True, video_callable=(lambda episode: episode % 10 == 0))
-        num_states = self.env.observation_space.shape[0]
-        num_actions = self.env.action_space.n
-        self.agent = Agent(num_states, num_actions)
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(obs_num, HIDDEN_SIZE)
+        self.fc2 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.fc3 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.fc4 = nn.Linear(HIDDEN_SIZE, acts_num)
 
-    def run(self):
-        episode_10_list = np.zeros(10)
-        complete_episodes = 0
-        before = time.time()
-        # 全エピソードループ
-        for episode in range(NUM_EPISODES):
-            # エピソード毎に環境を初期化
-            observation = self.env.reset()
-            state = observation
-            state = torch.from_numpy(state).type(torch.FloatTensor)
-            state = torch.unsqueeze(state, 0)
+    def __call__(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        h = F.relu(self.fc3(h))
+        y = F.relu(self.fc4(h))
+        return y
 
-            for step in range(MAX_STEP):
-                self.env.render()
-                action = self.agent.get_action(state, episode)
-                observation_next, reward, done, _ = self.env.step(action.item())
-                print('Episode: {0}, step: {1}, reward: {2}'.format(episode, step, reward))
-                # 報酬を与える
-                if done:
-                    state_next = None
-                    # 直前10エピソードで立てた平均ステップ数を格納
-                    episode_10_list = np.hstack((episode_10_list[1:], step+1))
-                    reward = torch.FloatTensor([reward])
-                    if step < MAX_STEP - 1:
-                        complete_episodes += 1
-                        break
-                    else:
-                        complete_episodes = 0
-                        break
-                else:
-                    reward = torch.FloatTensor([reward])
-                    state_next = observation_next
-                    state_next = torch.from_numpy(state_next).type(torch.FloatTensor)
-                    state_next = torch.unsqueeze(state_next, 0)
+# 定数
+EPOCH_NUM = 3000 # エポック数
+STEP_MAX = 200 # 最高ステップ数
+MEMORY_SIZE = 200 # メモリサイズいくつで学習を開始するか
+BATCH_SIZE = 50 # バッチサイズ
+EPSILON = 1.0 # ε-greedy法
+EPSILON_DECREASE = 0.001 # εの減少値
+EPSILON_MIN = 0.1 # εの下限
+START_REDUCE_EPSILON = 200 # εを減少させるステップ数
+TRAIN_FREQ = 10 # Q関数の学習間隔
+UPDATE_TARGET_Q_FREQ = 20 # Q関数の更新間隔
+GAMMA = 0.97 # 割引率
+LOG_FREQ = 1000 # ログ出力の間隔
 
-                self.agent.memorize(state, action, state_next, reward)
-                self.agent.update_q_function()
-                state = state_next
-                if done or step==MAX_STEP:
-                    print('{0}エピソード: {1}ステップで終了 - reward: {2}'.format(episode, step, reward))
-                    break
-            # 10回連続で成功したら終わりにする
-            if complete_episodes >= 10:
-                after = time.time()
-                exe_time = after - before
-                print('10回連続成功')
-                print('Execution Time: {0} sec'.format(round(exe_time, 5)))
-                return
+# モデル
+Q = NN() # 近似Q関数
+Q_ast = copy.deepcopy(Q)
+optimizer = optim.RMSprop(Q.parameters(), lr=0.00015, alpha=0.95, eps=0.01)
+total_step = 0 # 総ステップ（行動）数
+memory = [] # メモリ
+total_rewards = [] # 累積報酬記録用リスト
 
-cartpole_env = Environment()
-cartpole_env.run()
+# 学習開始
+print("Train")
+print("\t".join(["epoch", "EPSILON", "reward", "total_step", "elapsed_time"]))
+start = time.time()
+for epoch in range(EPOCH_NUM):
+    pobs = env.reset() # 環境初期化
+    step = 0 # ステップ数
+    done = False # ゲーム終了フラグ
+    total_reward = 0 # 累積報酬
+
+    while not done and step < STEP_MAX:
+        if MONITOR:
+            env.render()
+
+        # 行動選択
+        pact = env.action_space.sample()
+
+        # ε-greedy法
+        if np.random.rand() > EPSILON:
+
+            # 最適な行動を予測
+            pobs_ = np.array(pobs, dtype="float32").reshape((1, obs_num))
+            pobs_ = Variable(torch.from_numpy(pobs_))
+            pact = Q(pobs_)
+            maxs, indices = torch.max(pact.data, 1)
+            pact = indices.numpy()[0]
+
+        # 行動
+        obs, reward, done, _ = env.step(pact)
+        if done:
+            reward = -1
+
+        # メモリに蓄積
+        memory.append((pobs, pact, reward, obs, done)) # 状態、行動、報酬、行動後の状態、ゲーム終了フラグ
+        if len(memory) > MEMORY_SIZE: # メモリサイズを超えていれば消していく
+            memory.pop(0)
+
+        # 学習
+        if len(memory) == MEMORY_SIZE: # メモリサイズ分溜まっていれば学習
+
+            # 経験リプレイ
+            if total_step % TRAIN_FREQ == 0:
+                memory_ = np.random.permutation(memory)
+                memory_idx = range(len(memory_))
+                for i in memory_idx[::BATCH_SIZE]:
+                    batch = np.array(memory_[i:i+BATCH_SIZE]) # 経験ミニバッチ
+
+                    pobss = np.array(batch[:,0].tolist(), dtype="float32").reshape((BATCH_SIZE, obs_num))
+                    pacts = np.array(batch[:,1].tolist(), dtype="int32")
+                    rewards = np.array(batch[:,2].tolist(), dtype="int32")
+                    obss = np.array(batch[:,3].tolist(), dtype="float32").reshape((BATCH_SIZE, obs_num))
+                    dones = np.array(batch[:,4].tolist(), dtype="bool")
+
+                    # set y
+                    pobss_ = Variable(torch.from_numpy(pobss))
+                    q = Q(pobss_)
+                    obss_ = Variable(torch.from_numpy(obss))
+                    maxs, indices = torch.max(Q_ast(obss_).data, 1)
+                    maxq = maxs.numpy() # maxQ
+                    target = copy.deepcopy(q.data.numpy())
+                    for j in range(BATCH_SIZE):
+                        target[j, pacts[j]] = rewards[j]+GAMMA*maxq[j]*(not dones[j]) # 教師信号
+
+                    # Perform a gradient descent step
+                    optimizer.zero_grad()
+                    loss = nn.MSELoss()(q, Variable(torch.from_numpy(target)))
+                    loss.backward()
+                    optimizer.step()
+
+            # Q関数の更新
+            if total_step % UPDATE_TARGET_Q_FREQ == 0:
+                Q_ast = copy.deepcopy(Q)
+
+        # εの減少
+        if EPSILON > EPSILON_MIN and total_step > START_REDUCE_EPSILON:
+            EPSILON -= EPSILON_DECREASE
+
+        # 次の行動へ
+        total_reward += reward
+        step += 1
+        total_step += 1
+        pobs = obs
+
+    total_rewards.append(total_reward) # 累積報酬を記録
+    if (epoch+1) % LOG_FREQ == 0:
+        r = sum(total_rewards[((epoch+1)-LOG_FREQ):(epoch+1)])/LOG_FREQ # ログ出力間隔での平均累積報酬
+        elapsed_time = time.time()-start
+        print("\t".join(map(str,[epoch+1, EPSILON, r, total_step, str(elapsed_time)+"[sec]"]))) # ログ出力
+        start = time.time()
+
+if MONITOR:
+    env.render(close=True)
+
+plt.figure(figsize=(15,7))
+resize = (len(total_rewards)//10, 10)
+tmp = np.array(total_rewards, dtype="float32").reshape(resize)
+tmp = np.average(tmp, axis=1)
+plt.plot(tmp, color="cyan")
+plt.show()
